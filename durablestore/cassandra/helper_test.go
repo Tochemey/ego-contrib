b66d0ebecf776126c88b08d468f01cab3fb1c99b
Tochemey <tochemey@hotmail.com>
2026-01-01 22:19:40 +0000

    chore(copyright): update copyrights and license

--- durablestore/cassandra/helper_test.go
+++ durablestore/cassandra/helper_test.go
@@ -1,26 +1,24 @@
-/*
- * MIT License
- *
- * Copyright (c) 2024-2025 Arsene Tochemey Gandote
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
+// MIT License
+//
+// Copyright (c) 2024-2026 Arsene Tochemey Gandote
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 package cassandra
 
@@ -49,11 +47,11 @@ func NewTestContainer() *TestContainer {
 			Image:        "cassandra:5.0.6",
 			ExposedPorts: []string{"9042/tcp"},
 			Env: map[string]string{
-				"MAX_HEAP_SIZE":         "1G",
-				"HEAP_NEWSIZE":          "256M",
+				"MAX_HEAP_SIZE":          "1G",
+				"HEAP_NEWSIZE":           "256M",
 				"CASSANDRA_CLUSTER_NAME": "test",
-				"CASSANDRA_DC":          "dc1",
-				"CASSANDRA_RACK":        "rack1",
+				"CASSANDRA_DC":           "dc1",
+				"CASSANDRA_RACK":         "rack1",
 			},
 			WaitingFor: wait.ForListeningPort("9042/tcp").WithStartupTimeout(300 * time.Second),
 		},
